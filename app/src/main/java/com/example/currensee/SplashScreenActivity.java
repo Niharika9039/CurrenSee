928b812700a493a22b87e16addba250180da727d
Niharika <niharikasharma522@gmail.com>
2023-06-04 02:00:24 +0530

    second commit

--- app/src/main/java/com/example/currensee/SplashScreenActivity.java
+++ app/src/main/java/com/example/currensee/SplashScreenActivity.java
@@ -8,9 +8,12 @@ import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.VideoView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.currensee.MainActivity;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
@@ -40,10 +43,16 @@ public class SplashScreenActivity extends AppCompatActivity {
         Animation animation = AnimationUtils.loadAnimation(this, R.anim.fade_in);
         imageView.startAnimation(animation);
 
+        // Animate the text
+        Animation textAnimation = AnimationUtils.loadAnimation(this, R.anim.fade_in);
+        TextView animatedText = findViewById(R.id.animated_text);
+        animatedText.startAnimation(textAnimation);
+
         // Wait for the video to finish loading and the animation to complete
         long videoDuration = getVideoDuration(R.raw.splash_video);
         long animationDuration = animation.getDuration();
-        long totalDuration = Math.max(videoDuration, animationDuration);
+        long totalDuration = 6000; // Set the desired duration in milliseconds (e.g., 3000ms = 3 seconds)
+
 
         Thread timerThread = new Thread() {
             public void run() {
